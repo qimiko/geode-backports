8690ba8efbbc6cab731be50429f45685bc4890b0
Chloe <25387744+qimiko@users.noreply.github.com>
2025-07-18 17:42:37 -0700

    stop using geode build for windows

--- build.sh
+++ build.sh
@@ -8,6 +8,13 @@ outdir="$current/out/"
 rm -rf "$outdir"
 mkdir "$outdir"
 
+build_windows () {
+  cmake -B build-windows -DCMAKE_C_COMPILER=clang -DCMAKE_CXX_COMPILER=clang++ -DCMAKE_CXX_FLAGS=-m32 -DCMAKE_C_FLAGS=-m32 -G Ninja -DGEODE_DONT_INSTALL_MODS=ON -DCMAKE_BUILD_TYPE=RelWithDebInfo
+  cmake --build build-windows
+
+  return $?
+}
+
 for d in */
 do
   if [ ! -d "$d/clone/" ]
@@ -16,13 +23,20 @@ do
   fi
 
   pushd "$d/clone/"
-  geode build -p windows
-  geode build -p android32
-    
-  files=$(find build-android32 -name "*.geode" -type f -exec basename {} \;)
+  build_windows || {
+    echo "failed to build windows :("
+    exit 1
+  }
+  geode build -p android32 || {
+    echo "failed to build android :("
+    exit 1
+  }
+
+  files=$(find build-android32 -name "*.geode" -type f)
   for file in $files
   do
-    geode package merge "build-android32/$file" "build-windows/$file"
+    filename=$(basename "$file")
+    geode package merge "build-android32/$filename" "build-windows/$filename"
   done
   cp ./build-android32/*.geode "$outdir"    
   popd
