8a5c7da78336ad07a229db25eef94edee9fae67a
Chloe <25387744+qimiko@users.noreply.github.com>
2025-07-21 19:00:13 -0700

    add fallback if no identity set

--- setup.sh
+++ setup.sh
@@ -2,11 +2,18 @@
 
 shopt -s nullglob
 
+git_setup=true
+git config user.email
+if [ $? -ne 0 ]
+then
+  git_setup=false
+fi
+
 checkout_project () {
   d=$1
   if [ ! -f "$d/metadata" ]
   then
-    continue
+    return 1
   fi
 
   clone_url=$(sed -n "1p" "$d/metadata")
@@ -16,7 +23,12 @@ checkout_project () {
   git -c advice.detachedHead=false clone --revision=$commit_hash --depth=1 $clone_url "$d/clone/"
 
   pushd "$d/clone/" > /dev/null
-  git am -3 "../patch/"*.patch --ignore-whitespace --ignore-space-change
+  if $git_setup
+  then
+    git am -3 "../patch/"*.patch --ignore-whitespace --ignore-space-change
+  else
+    git apply "../patch"*.patch --ignore-whitespace --ignore-space-change
+  fi
   popd > /dev/null
 }
 
